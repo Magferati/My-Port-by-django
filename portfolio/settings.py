e468c64949d22ae95775225b7ec476f087c4330c
zannatul magferati <magferatizannatul@gmail.com>
2025-11-10 08:43:21 +0600

    Comment out CSRF_TRUSTED_ORIGINS setting
    Comment out CSRF_TRUSTED_ORIGINS for development.

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -38,7 +38,7 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 
 # development URL (optional)
-CSRF_TRUSTED_ORIGINS += ['http://127.0.0.1:8000', 'http://localhost:8000']
+#CSRF_TRUSTED_ORIGINS += ['http://127.0.0.1:8000', 'http://localhost:8000']
 
 
 
